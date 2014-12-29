2c032805d25eb2418be05bc3faa2f7fb39715884
John Robert Agate <johnagate@gmail.com>
2014-12-28 20:31:17 -0800

    first commit

---
+++ ExchangeAGram/FeedCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by John Robert Agate on 12/28/14.
+//  Copyright (c) 2014 John Robert Agate. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+}
